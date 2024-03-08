40a19f7d9727089384b3243264e6b4620e74baea
P Vaibhav <65724191+vaibhavprashanth@users.noreply.github.com>
2024-03-08 16:31:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World! I am P Vaibhav. My SRN is PES1UG21CS399";
+    std::cout << "Hello World! This is PES1UG21CS399";
     return 0;
 }
